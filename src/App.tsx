dd468d25d97477210d455bf93a9ee841530f45cd
Vladyslav Rozmarytsia <vladroz1995@gmail.com>
2025-04-14 15:31:49 +0300

    changes requested by AI

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import './App.scss';
 import { NavBar } from './components/NavBar';
+import { HashRouter as Router } from 'react-router-dom';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { HomePage } from './components/HomePage';
 import { PeoplePage } from './components/PeoplePage';
 import { PageNotFound } from './components/PageNotFound';
 
 export const App = () => (
-  <div data-cy="app">
-    <NavBar />
+  <Router>
+    <div data-cy="app">
+      <NavBar />
 
-    <main className="section">
-      <Routes>
-        <Route index element={<HomePage />} />
-        <Route path="/home" element={<Navigate to="/" />} />
-        <Route path="people">
-          <Route index element={<PeoplePage />} />
-          <Route path=":slug" element={<PeoplePage />} />
-        </Route>
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </main>
-  </div>
+      <main className="section">
+        <Routes>
+          <Route index element={<HomePage />} />
+          <Route path="/home" element={<Navigate to="/" />} />
+          <Route path="people">
+            <Route index element={<PeoplePage />} />
+            <Route path=":slug" element={<PeoplePage />} />
+          </Route>
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </main>
+    </div>
+  </Router>
 );
